55fd114bb3fb08197cc59ea4e77715c6df312772
Spacejellie <carterd19@montclair.edu>
2025-01-27 19:56:33 -0500

    Update
    With Script. Added movement and collisions

---
+++ 2DADVENTURE/Assets/Script/PlayerMove.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public GameObject player;
+    public float speed = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetKey(KeyCode.W))
+       {
+         player.transform.position += Vector3.up * speed;
+       }
+       if (Input.GetKey(KeyCode.S))
+       {
+         player.transform.position += Vector3.down * speed;
+       }
+       if (Input.GetKey(KeyCode.A))
+       {
+         player.transform.position += Vector3.left * speed;
+       }
+       if (Input.GetKey(KeyCode.D))
+       {
+         player.transform.position += Vector3.right * speed;
+       }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Key")
+        {
+          Destroy(collision.gameObject);
+        }
+        
+    }
+
+}
