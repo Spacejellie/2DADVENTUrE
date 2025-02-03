3e81201fbcd17c39ce006a0d4c82c065cebbd936
Spacejellie <carterd19@montclair.edu>
2025-02-03 05:23:51 -0500

    Update PlayerMov.cs

--- 2DADVENTURE/Assets/Script/PlayerMov.cs
+++ 2DADVENTURE/Assets/Script/PlayerMov.cs
@@ -7,49 +7,26 @@ using UnityEngine;
     public GameObject player;
     public float speed = 0.5f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-       if (Input.GetKey(KeyCode.W))
-       {
-         player.transform.position += Vector3.up * speed;
-         GameObject.SetBool("walk" , true);
-       }
-       else 
-       {
-        GameObject.SetBool("walk" , false);
-       }
-       if (input.GetKey(KeyCode.W)) &&(Input.GetKey(Keycode.Z))
-       {
-        GameObject.SetBool("walk" , false);
-        GameObject.SetBool("run" , true);
-       }
-       else 
-       {
-        GameObject.SetBool("run" , false );
-       }
-       if (Input.GetKey(KeyCode.S))
-       {
-         player.transform.position += Vector3.down * speed;
-       }
-       if (Input.GetKey(KeyCode.A))
-       {
-         player.transform.position += Vector3.left * speed;
-       }
-       if (Input.GetKey(KeyCode.D))
-       {
-         player.transform.position += Vector3.right * speed;
-       }
-       if (Input.GetKey(KeyCode.Z))
-       {
-        player.transform.position += Vector3.forward * Time.deltaTime * speed;
-       }
+        if (Input.GetKey(KeyCode.W))
+        {
+            player.transform.position += Vector3.up * speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            player.transform.position += Vector3.down * speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.transform.position += Vector3.left * speed;
+        }
+        if (!Input.GetKey(KeyCode.D))
+        {
+          player.transform.position += Vector3.right * speed;
+        }
+        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
